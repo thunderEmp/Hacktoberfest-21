6c1a9eebb2b9dd096ffb051da63846f38ddf8a13
parzival272000 <mayankjha272000@gmail.com>
2021-10-02 20:44:33 +0530

    Added Profile Mayank

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ var participants =
     "quote":"I ❤ code"
  },
  {  
-  "github":"Github Handle",
-  "name":"Name",
-  "intro":"Anything you want to share !!",
-  "quote":"quote ✨"
+  "github":"parzival27000",
+  "name":"Mayank Jha",
+  "intro":"Undergrad in pursuit of my goals",
+  "quote":"perseverance beats the tides of tough times ✨"
 },
 {  
   "github":"Github Handle",
